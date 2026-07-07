230e801a7eeddc82cf2085c9e78762cc0de46f04
Virianto <victor_nf@outlook.es>
2026-07-07 10:42:39 +0200

    · Imported InputSystem from _ReusableSystems

--- Assets/Code/Utils/Singleton.cs
+++ Assets/Code/Utils/Singleton.cs
@@ -1,47 +1,50 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// Be aware this will not prevent a non singleton constructor
-/// such as `T myT = new T();`
-/// To prevent that, add `protected T () {}` to your singleton class.
-/// </summary>
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+namespace Code.Utils
 {
-    #region ATTRIBUTES
-
-    static T _instance;
-
-    static object _lock = new object();
-
-    #endregion
-
-    #region METHODS
-
-    public static T Instance
+    /// <summary>
+    /// Be aware this will not prevent a non singleton constructor
+    /// such as `T myT = new T();`
+    /// To prevent that, add `protected T () {}` to your singleton class.
+    /// </summary>
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        get
-        {
-            lock (_lock)
-            {
-                if (_instance == null)
-                {
-                    _instance = FindAnyObjectByType<T>();
+        #region ATTRIBUTES
 
-                    if (FindObjectsByType<T>().Length > 1)
-                    {
-                        return _instance;
-                    }
+        static T _instance;
+
+        static object _lock = new object();
+
+        #endregion
+
+        #region METHODS
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
                     if (_instance == null)
                     {
-                        GameObject singleton = new GameObject();
-                        _instance = singleton.AddComponent<T>();
-                        singleton.name = "(Singleton) " + typeof(T).ToString();
+                        _instance = FindAnyObjectByType<T>();
+
+                        if (FindObjectsByType<T>().Length > 1)
+                        {
+                            return _instance;
+                        }
+                        if (_instance == null)
+                        {
+                            GameObject singleton = new GameObject();
+                            _instance = singleton.AddComponent<T>();
+                            singleton.name = "(Singleton) " + typeof(T).ToString();
+                        }
                     }
+                    return _instance;
                 }
-                return _instance;
             }
         }
-    }
 
-    #endregion
+        #endregion
+    }
 }
